16fad4cbcd02a0543467475ee514aefdd26fd36b
DanirPT <danir.hucaev@gmail.com>
2022-09-04 18:51:08 +0100

    renamed Main.java to Loser.java, made this class like servlet, added dependencies unit and servlet

---
+++ src/main/java/com/javarush/quest/Loser.java
@@ -0,0 +1,23 @@
+package com.javarush.quest;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/loser")
+public class Loser extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("<html>");
+        printWriter.println("Your scream has been heard, three hungry monster-dogs came to eat you.");
+        printWriter.println("<p>YOU LOSE<p>");
+        printWriter.println("<p>START AGAIN?</p>");
+        printWriter.println("  <button class=\"btn\" onclick=\"window.location='index.jsp'\">YES</button>\n");
+        printWriter.println("</html>");
+    }
+}
--- src/main/java/com/javarush/quest/Main.java
+++
@@ -1,4 +0,0 @@
-package com.javarush.quest;
-
-public class Main {
-}
